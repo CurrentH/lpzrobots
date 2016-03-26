186d2940b997d09f0b94a079c74a59a4654e548d
Mathias Thor <mathias_thor@hotmail.com>
2016-03-26 13:05:36 +0100

    It is now possible to change between the vel servo and the pos servo

--- ode_robots/motors/oneaxisservo.cpp
+++ ode_robots/motors/oneaxisservo.cpp
@@ -25,6 +25,9 @@
 #include "oneaxisservo.h"
 #include "joint.h"
 #include <selforg/controller_misc.h>
+#include <iostream>
+
+using namespace std;
 
 namespace lpzrobots {
 
@@ -167,6 +170,10 @@ namespace lpzrobots {
 	OneAxisServoVelPos::~OneAxisServoVelPos(){}
 
 	void OneAxisServoVelPos::set(double velocity){
+		if(abs(velocity) >= 9 && abs(velocity) <= 11){
+			set(velocity, true);
+		} else {
+		cout << "VELOCITY!" << endl;
 		velocity = clip(velocity, -1.0, 1.0);
 
 		// A function that scales the input to fit the maxPower
@@ -174,15 +181,21 @@ namespace lpzrobots {
 
 		// Pass the velocity directly
 		motor.set(0, velocity);
+		}
 	}
 
 	void OneAxisServoVelPos::set(double position, bool flag){
+
+		cout << "POSITION!" << endl;
+
+		position = position - 10; 					// CANCELS THE OFFSET
 		position = clip(position, -1.0, 1.0);
 		position = (position+1)*(max-min)/2 + min;
 
 		pid.setTargetPosition(position);
 		double velocity = pid.stepPositionVelocity( joint->getPosition1(), motor.get(0), joint->odeHandle.getTime() );
 
+		velocity = clip(velocity, -1.0, 1.0);		// Ensures no offset
 		set( velocity );
 
 
